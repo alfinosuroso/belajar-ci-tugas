0072a1af294dd436721acccffb315afab510cefe
Alfino Suroso <alfinosuroso@gmail.com>
2024-06-27 14:11:48 +0700

    feat: [AA] checkout and routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -27,4 +27,8 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('getcity', 'TransaksiController::getcity', ['filter' => 'auth']);
+$routes->get('getcost', 'TransaksiController::getcost', ['filter' => 'auth']);
+
 $routes->get('/contact', 'Home::viewKontak', ['filter' => 'auth']);
