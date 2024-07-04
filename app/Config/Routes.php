68660c55ca653c9d3f4efd2745c13cf4b795a28d
Alfino Suroso <alfinosuroso@gmail.com>
2024-07-04 14:04:43 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,3 +35,7 @@ $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('/contact', 'Home::viewKontak', ['filter' => 'auth']);
 
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+
+$routes->group('api', function ($routes) {
+    $routes->post('monthly', 'ApiController::monthly');
+});
